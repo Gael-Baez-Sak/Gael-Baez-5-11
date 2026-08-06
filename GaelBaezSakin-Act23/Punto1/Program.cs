bbc85ce527f4431472dbfb98610152ff8eccc8ef
Gael_Baez_Sak <gaelbaezsakin@gmail.com>
2026-08-06 10:02:39 -0300

    Add files via upload

---
+++ GaelBaezSakin-Act23/Punto1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    //    1-
+    //En un servidor de red, las solicitudes de impresión se procesan dinámicamente en orden de
+    //llegada.
+    // Crear la clase Documento que contenga como atributos privados:
+    //nombreArchivo(string) y cantidadPaginas(int). Definir sus
+    //propiedades de solo lectura y un constructor que reciba los parámetros nom
+    //y pag.
+    // Crear la clase ServidorImpresion que administre una lista dinámica de
+    //documentos (List<Documento>).
+    // Métodos en ServidorImpresion:
+    //1. AgregarDocumento() : Solicitar por teclado los datos de un
+    //documento y agregarlo al final de la lista utilizando.Add().
+    //2. ImprimirSiguiente() : Si la lista no está vacía, simular la
+    //impresión del primer documento de la lista(mostrar sus datos en
+    //consola) y removerlo de la colección mediante.RemoveAt(0). Si está
+    //vacía, advertir que no hay trabajos pendientes.
+    //3. MostrarColaPendiente(): Listar todos los documentos que
+    //están esperando ser impresos y la cantidad total de páginas
+    //acumuladas en la cola de espera utilizando .Count.
+    class Documento
+    {
+        private string nombreArchivo;
+        private int cantidadPaginas;
+        public string NombreArchivo
+        {
+            get { return nombreArchivo; }
+        }
+        public int CantidadPaginas
+        {
+            get { return cantidadPaginas; }
+        }
+        public Documento(string nom, int pag)
+        {
+            nombreArchivo = nom;
+            cantidadPaginas = pag;
+        }
+    }
+    class ServidorImpresion
+    {
+        private List<Documento> listaDocumentos;
+        public ServidorImpresion()
+        {
+            listaDocumentos = new List<Documento>();
+        }
+        public void AgregarDocumento()
+        {
+            Console.Write("Ingrese el nombre del archivo: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Ingrese la cantidad de páginas: ");
+            int paginas = int.Parse(Console.ReadLine());
+            Documento doc = new Documento(nombre, paginas);
+            listaDocumentos.Add(doc);
+            Console.WriteLine("Documento agregado a la cola de impresión.");
+        }
+        public void ImprimirSiguiente()
+        {
+            if (listaDocumentos.Count > 0)
+            {
+                Documento doc = listaDocumentos[0];
+            }
+        }
+        public void MostrarColaPendiente()
+        {
+            if (listaDocumentos.Count > 0)
+            {
+                Console.WriteLine("Documentos en la cola de impresión:");
+                int totalPaginas = 0;
+                foreach (Documento doc in listaDocumentos)
+                {
+                    Console.WriteLine($"Nombre: {doc.NombreArchivo}, Páginas: {doc.CantidadPaginas}");
+                    totalPaginas += doc.CantidadPaginas;
+                }
+                Console.WriteLine($"Cantidad total de documentos: {listaDocumentos.Count}");
+                Console.WriteLine($"Cantidad total de páginas: {totalPaginas}");
+            }
+            else
+            {
+                Console.WriteLine("No hay documentos en la cola de impresión.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ServidorImpresion si = new ServidorImpresion();
+
+            si.AgregarDocumento();
+            si.ImprimirSiguiente();
+            si.MostrarColaPendiente();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GaelBaezSakin-Act23/Punto4/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto4
+{
+    //    4-
+
+    //Un sistema central de domótica gestiona el consumo de los artefactos inteligentes
+    //vinculados a una red hogareña.
+    // Crear la clase DispositivoInteligente que contenga como atributos
+    //privados: nombreDispositivo(string) y consumoWatts(double). Definir
+    //sus propiedades y un constructor que reciba nom y watts.
+    // Crear la clase colaboradora PanelDomotico que administre un objeto
+    //List&lt;DispositivoInteligente&gt;.
+    // Métodos en PanelDomotico:
+    //1. Un constructor que permita al usuario cargar dinámicamente
+    //dispositivos por teclado.El sistema preguntará después de cada
+    //carga si se desea agregar otro dispositivo.
+    //2. MostrarDispositivos(): Listar todos los dispositivos
+    //configurados junto a sus consumos.
+    //3. CalcularConsumoTotal(): Calcular y mostrar en pantalla los
+    //Watts totales que consume la casa sumando los valores de la lista.
+    //4. DesconectarDispositivo(): Solicitar al usuario el nombre de
+    //un dispositivo y, si existe en la lista, removerlo de forma dinámica
+    //para simular su apagado remoto.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
