c43af38323e848c3507fbc2b2e9ef78ead26f5a6
Gael_Baez_Sak <gaelbaezsakin@gmail.com>
2026-08-20 09:29:10 -0300

    Add files via upload

---
+++ GaelBaezSakin-Act24/Punto7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Punto7
+{
+    internal class Program
+    {
+        //7-
+        //Un restaurante automatizado procesa la comanda de una mesa para controlar la
+        //preparación y el cobro de los platos pedidos.
+        // Crear la clase Plato que contenga como atributos privados: nombrePlato
+        //(string) y precio (double). Definir sus propiedades correspondientes y un
+        //constructor que reciba nom y pre.
+        // Crear la clase GestionComandas que administre una lista de objetos List.
+        // Métodos en GestionComandas:
+        //o AgregarPlato():Solicitar por teclado los datos de un plato y
+        //agregarlo a la lista utilizando .Add().
+        //o MostrarComanda(): Listar todos los platos agregados hasta el
+        //momento junto a la cantidad total de ítems pedidos utilizando la
+        //propiedad.Count.
+        //o CalcularTotalMesa(): Calcular y mostrar en pantalla el monto
+        //total a cobrar sumando los precios de la lista.
+        //o CancelarPlato(): Solicitar al usuario el nombre de un plato y, si
+        //se encuentra en la lista, removerlo mediante .Remove() para
+        //actualizar la comanda.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
